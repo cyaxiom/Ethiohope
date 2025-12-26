c2dd24b29c02a5efb64a66c1d9edcd85063ace95
temkin236 <temkin236@users.noreply.github.com>
2025-12-26 07:55:44 +0300

    colour theme ,for all page and nav link

--- src/pages/Services/EthiohopeService/EthiohopeService.jsx
+++ src/pages/Services/EthiohopeService/EthiohopeService.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import Hero from "../../../components/Services/EthiohopeService/Hero";
+import ClientLogos from "../../../components/Services/EthiohopeService/ClientLogos";
+import RecentSEOWork from "../../../components/Services/EthiohopeService/RecentSEOWork";
+import AboutUs from "../../../components/Services/EthiohopeService/AboutUs";
+import SecurityFeatures from "../../../components/Services/EthiohopeService/SecurityFeatures";
+import SecurityWorkflow from "../../../components/Services/EthiohopeService/SecurityWorkflow";
+import GlobalSecurity from "../../../components/Services/EthiohopeService/GlobalSecurity";
+import IndustriesSection from "../../../components/Services/EthiohopeService/IndustriesSection";
+import CustomerStories from "../../../components/Services/EthiohopeService/CustomerStories";
 
 function EthiohopeService() {
   return (
