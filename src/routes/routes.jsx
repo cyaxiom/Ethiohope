b63e8dad584ce02cfee489335640ff9eb57370a7
Alehegne <alehegngetaenyewuu@gmail.com>
2026-01-23 23:25:21 +0300

    add video-call, audio-call routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -24,8 +24,8 @@ const ForumLogin = React.lazy(() => import('@components/Forum/Login'));
 const Tags = React.lazy(() => import('@components/Forum/Tags'));
 const Ranking = React.lazy(() => import('@components/Forum/Ranking'));
 const Questions = React.lazy(() => import('@components/Forum/Questions'));
-const QuestionDetails = React.lazy(() =>
-  import('@components/Forum/QuestionDetails')
+const QuestionDetails = React.lazy(
+  () => import('@components/Forum/QuestionDetails'),
 );
 const MyQuestions = React.lazy(() => import('@components/Forum/MyQuestions'));
 const MyAnswers = React.lazy(() => import('@components/Forum/MyAnswers'));
@@ -34,47 +34,59 @@ const ForumProfile = React.lazy(() => import('@components/Forum/ForumProfile'));
 
 //dashboard routes
 const Chats = React.lazy(() => import('@pages/Dashboard/Chats'));
+const VideoCall = React.lazy(() => import('@pages/Dashboard/VideoCall'));
+const VoiceCall = React.lazy(() => import('@pages/Dashboard/VoiceCall'));
 
 // Academy routes
-const KidsProgramming = React.lazy(() =>
-  import('@pages/Aademy/KidsProgramming/KidsProgramming')
+const KidsProgramming = React.lazy(
+  () => import('@pages/Aademy/KidsProgramming/KidsProgramming'),
 );
 
-const KidsTutorial = React.lazy(() =>
-  import('@pages/Aademy/KidsTutorial/KidsTutorial')
+const KidsTutorial = React.lazy(
+  () => import('@pages/Aademy/KidsTutorial/KidsTutorial'),
 );
-const AllKidsCourses = React.lazy(() =>
-  import('@pages/Aademy/KidsProgramming/components/AllCourses')
+const AllKidsCourses = React.lazy(
+  () => import('@pages/Aademy/KidsProgramming/components/AllCourses'),
 );
-const Web3Development = React.lazy(() =>
-  import('@pages/Aademy/Web3Development/Web3Development')
+const Web3Development = React.lazy(
+  () => import('@pages/Aademy/Web3Development/Web3Development'),
 );
-const FullStackDev = React.lazy(() =>
-  import('@pages/Aademy/FullStackDev/FullStackDev')
+const FullStackDev = React.lazy(
+  () => import('@pages/Aademy/FullStackDev/FullStackDev'),
 );
 // Services routes
-const EthiohopeServices = React.lazy(() =>
-  import('@pages/Services/EthiohopeService/EthiohopeService')
+const EthiohopeServices = React.lazy(
+  () => import('@pages/Services/EthiohopeService/EthiohopeService'),
 );
-const ProgrammingService = React.lazy(() =>
-  import('@pages/Services/ProgrammingService/ProgrammingService')
+const ProgrammingService = React.lazy(
+  () => import('@pages/Services/ProgrammingService/ProgrammingService'),
 );
 
-const KidsTutoring = React.lazy(() =>
-  import('@pages/Services/KidsTutoring/KidsTutoring')
+const KidsTutoring = React.lazy(
+  () => import('@pages/Services/KidsTutoring/KidsTutoring'),
 );
 // const ProjectServices = React.lazy(() =>
 //   import('@pages/Services/ProjectServices/ProjectServices')
 // );
 
-const CourseDetail = React.lazy(() =>
-  import('@pages/Aademy/KidsProgramming/components/CourseDetail')
+const CourseDetail = React.lazy(
+  () => import('@pages/Aademy/KidsProgramming/components/CourseDetail'),
 );
 
 export const routes = [
   { path: '/', exact: true, name: 'Home', element: <Home /> },
   //dashboard routes
   { path: '/dashboard/chats', name: 'Chats', element: <Chats /> },
+  {
+    path: '/dashboard/chats/video-call',
+    name: 'VideoCall',
+    element: <VideoCall />,
+  },
+  {
+    path: '/dashboard/chats/voice-call',
+    name: 'VoiceCall',
+    element: <VoiceCall />,
+  },
   // About routes
   { path: '/about', name: 'About', element: <About /> },
   { path: '/company', name: 'Company', element: <Company /> },
